6fb257425225fb4dfe3333f6e1afd467bf61e550
Mike Paiva <mikepaiva@outlook.com>
2020-03-07 16:20:32 -0500

    Quick app.js cleanup

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-
+// CONFIGS
 let express = require("express"),
 app = express(),
 bodyParser = require("body-parser");
 const port = 3000;
-
-// Landing Page
-app.get("/", function(req, res){
-	res.render("index"); // CHANGE TO LANDING ONCE LANDING IS INCORPORATED!!!
-});
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-// Landing Page
+// RENDER STARTING PAGE
 app.get("/", function(req, res){
-	res.render("index");
+	res.render("index"); // CHANGE TO LANDING ONCE LANDING IS INCORPORATED!!!
 });
-
 
 // START THE SERVER
 app.listen(port, () => console.log(`app listening on port ${3000}!`));
